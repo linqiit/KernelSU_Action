e6e4d4d6dc038185549aadab91c420498e069bc7
linqiit <3379328034@qq.com>
2024-11-14 22:53:00 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -227,7 +227,7 @@ lxc_docker() {
         grep -q "CONFIG_ANDROID_PARANOID_NETWORK" arch/$ARCH/configs/$KERNEL_CONFIG && sed -i 's/CONFIG_ANDROID_PARANOID_NETWORK=y/# CONFIG_ANDROID_PARANOID_NETWORK is not set/' arch/$ARCH/configs/$KERNEL_CONFIG
         aria2c https://github.com/dabao1955/kernel_build_action/raw/main/lxc/cgroup.patch
         # patch kernel/cgroup.c < cgroup.patch
-        patch $WORK/$KERNEL_DIR/kernel/cgroup.c < cgroup.patch && rm –rf cgroup.path
+        patch $WORK/$KERNEL_DIR/kernel/cgroup.c < cgroup.patch && rm -f cgroup.path
         # patch -p0 < cgroup.patch
         # aria2c https://github.com/dabao1955/kernel_build_action/raw/main/lxc/xt_qtaguid.patch
         # patch net/netfilter/xt_qtaguid.c < xt_qtaguid.patch
